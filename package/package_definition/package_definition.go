a6254bcdb74c8d3271294f5af3ea8400ed0064d0
fang16 <1877036647@163.com>
2021-11-19 15:17:24 +0800

    go 包定义

---
+++ package/package_definition/package_definition.go
@@ -0,0 +1,15 @@
+package package_definition
+
+
+import "fmt"
+	//go 模块中要导出的函数，包名首字母必须大写
+	const Verdion = 23112222
+    const Atest = "test"
+	//
+
+
+  func main() {
+	testA := []int{1,2,3,4}
+
+	fmt.Println(testA)
+}
